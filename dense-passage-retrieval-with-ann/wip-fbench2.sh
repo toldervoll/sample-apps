d1ec3869cde8ceb4a9453cc41968c4b60a6a0ca2
Tor Egge <Tor.Egge@verizonmedia.com>
2021-02-22 15:18:05 +0000

    Use -c 0. Adjust timeout.

--- dense-passage-retrieval-with-ann/wip-fbench2.sh
+++ dense-passage-retrieval-with-ann/wip-fbench2.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 clients=${1:-1}
-vespa-fbench -P -n $clients -c 1 -z -r 0 -s -1 -p 20 -q fbench-queries2 -H "Content-Type: application/json" localhost 8080
+if test "$clients" eq 1
+then
+  timeout=-1
+else
+  timeout=20
+fi
+vespa-fbench -P -n $clients -c 0 -z -r 0 -s $timeout -p 20 -q fbench-queries2 -H "Content-Type: application/json" localhost 8080
