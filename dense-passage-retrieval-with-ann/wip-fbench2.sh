7e6c8dd71ae4e668d6ef8187146e55f8f42741fd
Tor Egge <Tor.Egge@verizonmedia.com>
2021-02-24 15:06:06 +0000

    Always 60 seconds timeout

--- dense-passage-retrieval-with-ann/wip-fbench2.sh
+++ dense-passage-retrieval-with-ann/wip-fbench2.sh
@@ -2,10 +2,5 @@
 
 clients=${1:-1}
 suffix=$2
-if test "$clients" -eq 1
-then
-  timeout=-1
-else
-  timeout=60
-fi
+timeout=60
 vespa-fbench -P -n $clients -c 0 -z -r 0 -s $timeout -p 20 -q fbench-queries2${suffix:+-}$suffix -H "Content-Type: application/json" localhost 8080
