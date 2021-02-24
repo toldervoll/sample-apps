5c0da2f141bbf9d1f8d41697eaf774e7c71932e5
Tor Egge <Tor.Egge@verizonmedia.com>
2021-02-24 12:38:39 +0000

    Make fbench queries with 10+490, 10 and 100 targetHits.

--- dense-passage-retrieval-with-ann/wip-fbench2.sh
+++ dense-passage-retrieval-with-ann/wip-fbench2.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 clients=${1:-1}
+suffix=$2
 if test "$clients" -eq 1
 then
   timeout=-1
 else
-  timeout=20
+  timeout=60
 fi
-vespa-fbench -P -n $clients -c 0 -z -r 0 -s $timeout -p 20 -q fbench-queries2 -H "Content-Type: application/json" localhost 8080
+vespa-fbench -P -n $clients -c 0 -z -r 0 -s $timeout -p 20 -q fbench-queries2${suffix:+-}$suffix -H "Content-Type: application/json" localhost 8080
